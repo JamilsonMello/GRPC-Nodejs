994341f0d7d3a1b19aa79257edc1cdd2b6c93e1f
jamilson <jamilsonmelow@gmail.com>
2021-11-26 19:37:47 -0300

    create fake database

---
+++ data/fakeDatabase.js
@@ -0,0 +1,17 @@
+export default [
+    {
+        id: 1,
+        title: 'Test',
+        description: 'descrição'
+    },
+    {
+        id: 2,
+        title: 'Test2',
+        description: 'descrição2'
+    },
+    {
+        id: 3,
+        title: 'Test3',
+        description: 'descrição3'
+    },
+]
